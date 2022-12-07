3108c1ee170cf5faa6133e34f5b1de4cafa763cf
mustafa <muhammdmustafa5762@gmail.com>
2022-12-08 03:12:19 +0500

    s

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return <AnimatePresence> <motion.div key={router.route} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}> <Component {...pageProps} /> </motion.div> </AnimatePresence>
 }
 
 export default MyApp
