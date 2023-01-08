f4dbab2cbb1febb032c5d1b37ad465623ea3fbb7
mustafa <muhammdmustafa5762@gmail.com>
2023-01-08 05:40:18 +0500

    sc

--- components/Blogpost.jsx
+++ components/Blogpost.jsx
@@ -11,14 +11,19 @@ function Blogpost({blog,openit}) {
     <>
     { blog && <div className='mt-32 px-4 lg:px-32 flex flex-col items-center'>
       <div className="max-w-4xl">
-      <h1 className="font-bold text-3xl lg:text-4xl text-slate-900">Brooches - Decorative Pins, Clasps, & Kilt Brooches</h1>
-      <div className="text-xl lg:text-2xl text-slate-700 mt-2">A brooch is a decorative pin or clasp, usually made of metal, plastic, shell, precious stone or wood</div>
+      <div className="flex items-center gap-x-4 mb-6">
+        <div className="bg-indigo-50 text-indigo-700 px-2 py-1 text-sm cursor-pointer hover:bg-indigo-100 transition rounded-xl">Gaming</div>
+        <div className="bg-amber-50 text-amber-700 px-2 py-1 text-sm cursor-pointer hover:bg-amber-100 transition rounded-xl">Lifestyle</div>
+        <div className="bg-green-50 text-green-700 px-2 py-1 text-sm cursor-pointer hover:bg-green-100 transition rounded-xl">Shopping</div>
+      </div>
+      <h1 className="font-extrabold tracking-normal text-3xl lg:text-4xl text-slate-900">Brooches - Decorative Pins, Clasps, & Kilt</h1>
+      <div className="text-lg lg:leading-normal lg:text-2xl text-slate-700 mt-2">A brooch is a decorative pin or clasp, usually made of metal, plastic, shell, precious stone or wood</div>
       <div className="flex justify-end mt-2">
         <Icons likess={blog.likes} id={blog._id} blog={blog} openit={openit} comment={true}/>
       </div>
         <img className='mt-6 w-full rounded-md' src="https://ncmaz.chisnghiax.com/wp-content/uploads/2021/09/pexels-photo-3800412-1.jpeg" alt="" />
         <br /><br />
-          <Markdown className="prose prose-md lg:prose-xl prose-p:text-slate-600 prose-pre:p-0 prose-pre:m-0 prose-headings:mb-5" options={{ wrapper: 'article' , overrides: {code: {component: High}} }}>
+          <Markdown className="prose prose-lg lg:prose-xl prose-slate prose-pre:p-0 prose-pre:m-0 prose-headings:mb-5" options={{ wrapper: 'article' , overrides: {code: {component: High}} }}>
             {blog.description}
           </Markdown>
           <div className="Divider"></div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,16 +11,19 @@ function Navbar({setlog}) {
   return (
     <div>
         <div className="flex justify-between items-center h-20 px-5 lg:px-32 border-b border-gray-300">
-          <div className='text-gray-700'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
+          <div className="text-2xl font-black text-gray-900">READER</div>
+          <div className="flex items-center gap-x-8">
+            <div className="text-gray-500 font-semibold tracking-wider">Home</div>
+            <div className="text-gray-500 font-semibold tracking-wider">About</div>
+            <div className="text-gray-500 font-semibold tracking-wider">Lifestyle</div>
+            <div className="text-gray-500 font-semibold tracking-wider">Shopping</div>
+            <div className="text-gray-500 font-semibold tracking-wider">Tutorials</div>
+            <div className="text-gray-500 font-semibold tracking-wider">Templates</div>
           </div>
-          <div className="text-2xl font-extrabold text-gray-900">READER</div>
           { user &&  <div className='flex gap-6 items-center'>
             <div className="text-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
               </svg>
             </div>
             <div className="relative">
@@ -50,7 +53,7 @@ function Navbar({setlog}) {
           {
             !user &&
             <div>
-              <button onClick={() => setlog(true)} type="button" class="text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full px-5 py-2.5 tracking-wider">Get Started</button>
+              <button onClick={() => setlog(true)} type="button" class="text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full px-5 py-2.5 tracking-wide">Get Started</button>
             </div>
           }
         </div>
